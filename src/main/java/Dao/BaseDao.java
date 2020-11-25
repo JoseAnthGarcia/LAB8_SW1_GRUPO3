a72e059dac3d6614493a0617f65e5b234658f539
Jose Garcia <garcia.josea@pucp.edu.pe>
2020-11-24 19:13:12 -0500

    daobase cambio nombre de bd

--- src/main/java/Dao/BaseDao.java
+++ src/main/java/Dao/BaseDao.java
@@ -14,7 +14,7 @@ public abstract class BaseDao {
 
         String user = "root";
         String password = "root";
-        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
+        String url = "jdbc:mysql://localhost:3306/sw1lab8?serverTimezone=America/Lima";
 
         return DriverManager.getConnection(url, user, password);
     }
