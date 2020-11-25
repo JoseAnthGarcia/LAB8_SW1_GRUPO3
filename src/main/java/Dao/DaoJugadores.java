84ef0b6409ab7a44f499e5ff0433680b629ae6ba
Katherine Obregon <a20181823@pucp.edu.pe>
2020-11-24 19:17:20 -0500

    lista jugadores

--- src/main/java/Dao/DaoJugadores.java
+++ src/main/java/Dao/DaoJugadores.java
@@ -9,9 +9,38 @@ import java.util.ArrayList;
 public class DaoJugadores {
 
     public ArrayList<Jugadores> listarJugadores(){
-                /*
-                Inserte su código aquí
-                 */
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String usuario="root";
+        String password="root";
+        String url="jdbc:mysql://localhost:3306/hr?serverTimezone=America/Lima";
+
+        ArrayList<Jugadores> listaJugadores=  new ArrayList<>();
+        String sql= "SELECT * FROM sw1lab8.jugadores;";
+
+        try (Connection connection= DriverManager.getConnection(url,usuario,password);
+             Statement statement = connection.createStatement();
+             ResultSet rs= statement.executeQuery(sql)){
+            while(rs.next()){
+                Jugadores jugadores = new Jugadores();
+                jugadores.setIdJugadores(rs.getInt(1));
+                jugadores.setNombre(rs.getString(2));
+                jugadores.setEdad(rs.getInt(3));
+                jugadores.setPosicion(rs.getString(4));
+                jugadores.setClub(rs.getString(5));
+                jugadores.setSn_idSeleccion(rs.getInt(6));
+
+                listaJugadores.add(jugadores);
+            }
+
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return listaJugadores;
     }
 
     public Jugadores buscarJugadore(int id){
